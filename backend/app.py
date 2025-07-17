0ea8b71d5941653eeb005c45478511fe1cf8abe3
ParthKajavadra <parthkajavadra1998@gmaol.com>
2025-07-17 17:59:51 +0200

    backend and frontend setup

--- backend/app.py
+++ backend/app.py
@@ -18,4 +18,4 @@ def handle_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5050)
